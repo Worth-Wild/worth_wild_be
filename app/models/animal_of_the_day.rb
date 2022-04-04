7222d1ce061e801fabe349a80a0ff993e37a91b1
Joel Grant <joelmgrant@me.com>
2022-04-03 18:50:15 -0600

    Modify logic for adding an animal of the day

--- app/models/animal_of_the_day.rb
+++ app/models/animal_of_the_day.rb
@@ -2,13 +2,20 @@ class AnimalOfTheDay < ApplicationRecord
   validates :common_name, presence: true
   validates :element_code, presence: true
   validates :scientific_name, presence: true
+  validates :date, presence: true
 
   def self.get_animal_of_the_day
-    animal = AnimalOfTheDay.where(created_at: Date.today)
+    animal = AnimalOfTheDay.where(date: Date.today).first
 
     if !animal.present?
       animal = SearchAnimal.all.sample
+      AnimalOfTheDay.create(
+        element_code: animal.element_code,
+        common_name: animal.common_name,
+        scientific_name: animal.scientific_name,
+        date: Date.today
+      )
     end
-    new_animal_of_the_day = AnimalOfTheDay.create(animal.attributes)
+    AnimalOfTheDay.last
   end
 end
