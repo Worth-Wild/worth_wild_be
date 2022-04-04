0bdcab1eec604b57651240afd32b3170dc58ceb4
Joel Grant <joelmgrant@me.com>
2022-04-03 18:15:24 -0600

    Remove pry and incorrect assertions

--- spec/requests/api/v1/animal_of_the_day_request_spec.rb
+++ spec/requests/api/v1/animal_of_the_day_request_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Animal of the Day Endpoint' do
   describe 'GET /animal_of_the_day' do
     it 'returns a random animal as the animal of the day' do
       get '/api/v1/animal_of_the_day'
-      # require 'pry'; binding.pry
+
       data = JSON.parse(response.body, symbolize_names: true)
 
       expect(data).to be_a Hash
@@ -12,7 +12,6 @@ RSpec.describe 'Animal of the Day Endpoint' do
       expect(data[:attributes]).to be_a Hash
 
       expect(data[:attributes]).to have_key(:common_name)
-      expect(data[:attributes][:common_name]).to be_a String
 
       expect(data[:attributes]).to have_key(:scientific_name)
       expect(data[:attributes][:scientific_name]).to be_a String
@@ -39,19 +38,14 @@ RSpec.describe 'Animal of the Day Endpoint' do
       expect(data[:attributes][:vulnerability]).to be_a Hash
 
       expect(data[:attributes]).to have_key(:habitats)
-      expect(data[:attributes][:habitats]).to be_a String
 
       expect(data[:attributes]).to have_key(:habitatComments)
-      expect(data[:attributes][:habitatComments]).to be_a String
 
       expect(data[:attributes]).to have_key(:threatImpactComments)
-      expect(data[:attributes][:threatImpactComments]).to be_a String or nil
 
       expect(data[:attributes]).to have_key(:shortTermTrend)
-      expect(data[:attributes][:shortTermTrend]).to be_a String or nil
 
       expect(data[:attributes]).to have_key(:popSize)
-      expect(data[:attributes][:popSize]).to be_a String
 
       expect(data[:attributes]).to have_key(:imageUrl)
       expect(data[:attributes][:imageUrl]).to be_a String
