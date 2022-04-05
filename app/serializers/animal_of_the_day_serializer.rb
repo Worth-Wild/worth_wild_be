ff36801c5efcad625d9905ccbc33256befd25578
Joel Grant <joelmgrant@me.com>
2022-04-05 15:51:18 -0600

    Modify serializer variables

--- app/serializers/animal_of_the_day_serializer.rb
+++ app/serializers/animal_of_the_day_serializer.rb
@@ -1,6 +1,6 @@
 class AnimalOfTheDaySerializer
 
-def self.format_animal( photo, animal_of_the_day)
+def self.format_animal(animal_of_the_day)
   {
   "data":
    {
@@ -11,7 +11,7 @@ def self.format_animal( photo, animal_of_the_day)
         "common_name": animal_of_the_day.common_name,
         "scientific_name": animal_of_the_day.scientific_name,
         "element_code": animal_of_the_day.element_code,
-        "imageUrl": photo.photo_url,
+        "imageUrl": animal_of_the_day.image,
       }
     }
   }
